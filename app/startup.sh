86acdf47dd7b50553e382363ba9d24a000a78263
Khanh Hong Pham <khanh.hong.pham@mgm-tp.com>
2019-06-09 14:28:53 +0700

    Fixing cloudsql proxy issue

--- app/startup.sh
+++ app/startup.sh
@@ -1,6 +1,5 @@
 # if using cloudsql
 if [ ! -z "$CLOUDSQL_INSTANCE" ]; then
-    wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
     chmod +x ./cloud_sql_proxy
     ./cloud_sql_proxy -instances=$CLOUDSQL_INSTANCE=tcp:5432 -credential_file=/opt/file.json &
     sleep 5
